28ff81b398f11d1a14b144a5b8ef11a0041839bb
Yoonseok Hur <yoonseok@gmail.com>
2024-02-15 12:09:09 +0900

    home doc enabled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,28 @@
-import React  from 'react';
+import React, {useState, useEffect} from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import MemoryIcon from '@mui/icons-material/Memory';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 const Home = (props) => {
+    const [doc, setDoc] = useState();
+
+    useEffect(() => {
+        let url = 'https://raw.githubusercontent.com/io7lab/io7-platform-cloud/main/doc/io7intro.html';
+
+        fetch(url, {
+            method: 'get'
+        }).then((response) => {
+            return response.text();
+        }).then((txt) => {
+            setDoc(txt);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, []);
 
     return (
-        <iframe src="/intro.html" frameborder="0" marginheight="0" marginwidth="0" width="1000px" height="100%"></iframe>
+        <iframe srcdoc={doc} frameborder="0" marginheight="0" marginwidth="0" width="1000px" height="100%"></iframe>
     )
 }
 
