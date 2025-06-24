ec99bab5b5cc13be0c4b1657b6c7e43dc396032d
Yoonseok Hur <yoonseok@gmail.com>
2025-06-24 23:39:28 +0900

    Home with other console url

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import MemoryIcon from '@mui/icons-material/Memory';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
@@ -11,7 +11,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box } from '@mui/material';
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+import SchemaIcon from '@mui/icons-material/Schema';
+import StorageIcon from '@mui/icons-material/Storage';
+import ApiIcon from '@mui/icons-material/Api';
+import { Box, Tooltip } from '@mui/material';
 
 const svr = window.location;
 let rootURL = window.runtime.API_URL_ROOT || svr.protocol+'//'+svr.hostname+':2009';
@@ -22,6 +26,15 @@ const Home = (props) => {
     const [ dashboard, setDashboard ] = useState([]);
     const [ appIds, setAppIds ] = useState([]);
     const [ devices, setDevices] = useState([]);
+    const windowRefs = useRef({});
+    const nodered_name = 'NodeRed';
+    const nodered_url = window.runtime.NODERED_URL || 'http://localhost:1880';
+    const grafana_name = 'Grafana';
+    const grafana_url = window.runtime.GRAFANA_URL || 'http://localhost:3003';
+    const influxdb_name = 'InfluxDB';
+    const influxdb_url = window.runtime.INFLUXDB_URL || 'http://localhost:8086';
+    const apiserver_name = 'API Server';
+    const apiserver_url = rootURL;
 
     useEffect(() => {
         fetch(rootURL + '/config/dashboard', {
@@ -92,10 +105,46 @@ const Home = (props) => {
         }
     ));
 
+    const launchConsole = (name, url) => {
+        const windowName = name;
+
+        let winRef = windowRefs.current[windowName];
+
+        if (winRef && !winRef.closed) {
+            winRef.focus();
+            return;
+        }
+
+        winRef = window.open(url, windowName);
+        if (winRef) {
+            windowRefs.current[windowName] = winRef;
+            winRef.focus();
+        }
+    };
+
     return (
         <div style={{ height: '100%', marginLeft: '5%', overflowY: 'hidden' }}>
             <center>
-                <h1>io7 IOT Device Platform</h1><hr />
+                <h1>io7 IOT Device Platform</h1>
+                <Box sx={{ color: '#1a237e', display: 'flex', gap: 10, justifyContent: 'center' }}>
+                    <Tooltip title={nodered_name}><SchemaIcon fontSize='large'
+                        onClick={() => launchConsole(nodered_name, nodered_url)} 
+                        sx={{ cursor: 'pointer' }} onMouseUp={(e) => e.stopPropagation()}
+                    /></Tooltip> 
+                    <Tooltip title={grafana_name}><AutoGraphIcon fontSize='large' 
+                        onClick={() => launchConsole(grafana_name, grafana_url)} 
+                        sx={{ cursor: 'pointer' }} onMouseUp={(e) => e.stopPropagation()}
+                    /></Tooltip>
+                    <Tooltip title={influxdb_name}><StorageIcon fontSize='large'
+                        onClick={() => launchConsole(influxdb_name, influxdb_url)} 
+                        sx={{ cursor: 'pointer' }} onMouseUp={(e) => e.stopPropagation()}
+                    /></Tooltip>
+                    <Tooltip title={apiserver_name}><ApiIcon fontSize='large'
+                        onClick={() => launchConsole(apiserver_name, apiserver_url)} 
+                        sx={{ cursor: 'pointer' }} onMouseUp={(e) => e.stopPropagation()}
+                    /></Tooltip>
+                </Box>
+                <hr/>
                 <iframe
                     src={dashboard}
                     width="80%" height="450" frameBorder="0" />
@@ -155,12 +204,6 @@ const Home = (props) => {
                         </Table>
                     </TableContainer>
                 </div>
-                <Box sx={{ mt: 10 }}>
-                    <HomeIcon fontSize='large' />
-                    <MemoryIcon fontSize='large' />
-                    <AppRegistrationIcon fontSize='large' />
-                    <SettingsIcon fontSize='large' />
-                </Box>
             </center>
 
         </div>
